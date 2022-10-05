aa9626e62ea9f48f7e6aec8b8457a53101c95012
Stefan Koev <st.koev@outlook.com>
2022-10-05 12:53:43 +0300

    implement fetch a joke, final commit

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('Dad Jokes Starter');
+// console.log("Dad Jokes Starter");
+// html select elements
+const result = document.querySelector(".result");
+const btn = document.querySelector(".btn");
+
+const url = "https://icanhazdadjoke.com/";
+
+async function getJoke(url) {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "learning app",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error occured: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.joke;
+  } catch (error) {
+    return error;
+  }
+}
+
+btn.addEventListener("click", async () => {
+  result.textContent = "Loading a joke...";
+  result.textContent = await getJoke(url);
+});
+
+window.addEventListener("DOMContentLoaded", async () => {
+  result.textContent = "Loading a joke....";
+  result.textContent = await getJoke(url);
+});
